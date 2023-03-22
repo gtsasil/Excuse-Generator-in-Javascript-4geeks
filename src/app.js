cde297142fb024d8cf09b5d5351bad86491bc456
Gleisson Santos Silva <gtsasil@gmail.com>
2023-03-22 12:39:22 +0000

    End of exer

--- src/app.js
+++ src/app.js
@@ -4,5 +4,30 @@ import "./style.css";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  whoIndex = Math.floor(Math.random() * who.length);
+  actionIndex = Math.floor(Math.random() * action.length);
+  whatIndex = Math.floor(Math.random() * what.length);
+  whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex] +
+    " "
+  );
 };
